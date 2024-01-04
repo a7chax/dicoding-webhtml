abacd6c1b27aca0c8e4aeb81a4b05d885a4a8bc1
arungicahaya <akuarung83@gmail.com>
2024-01-05 01:58:29 +0700

    dom manipulation

---
+++ scripts/welcome.js
@@ -0,0 +1,11 @@
+// function alert(nama) {
+//   console.log('Hati-hati, ' + nama);
+// }
+ 
+// alert('Chewbacca'); // Output: Hati-hati, Chewbacca
+// Output di atas akan tercetak ke console browser
+ 
+// window.alert('Chewbacca'); // Output: Hati-hati, Chewbacca
+
+const tamu = prompt('Siapakah Anda?');
+alert('Selamat datang ' + tamu + '!');
