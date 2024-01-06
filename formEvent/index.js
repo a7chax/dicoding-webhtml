a852e14215f48e76a52f2d334a00790ef9da4032
arungicahaya <akuarung83@gmail.com>
2024-01-06 13:56:54 +0700

    input event

--- formEvent/index.js
+++ formEvent/index.js
@@ -32,5 +32,39 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('inputNama: blur');
     document.getElementById('notifikasiSisaKarakter').style.visibility = 'hidden';
   });
+
+  document.getElementById('inputCaptcha').addEventListener('change', function () {
+    console.log('inputCabang: change');
+
+    const  inputCaptcha = document.getElementById('inputCaptcha').value;
+    const submitButtonStatus = document.getElementById('submitButton');
+
+    if (inputCaptcha === 'PRNU') {
+      submitButtonStatus.removeAttribute('disabled');
+    } else {
+      submitButtonStatus.setAttribute('disabled', '');
+    }
+  })
+
+  document.getElementById('formDataDiri').addEventListener('submit', function(event){
+    const inputCaptcha = document.getElementById('inputCaptcha').value;
+
+    if(inputCaptcha === 'PRNU'){
+      alert('Selamat! Captcha anda benar');    
+    }else{
+      alert('Captcha anda salah');
+      document.getElementById('submitButton').setAttribute('disabled', '');
+    }
+    event.preventDefault()
+  })
+
+  document.getElementById('inputCopy').addEventListener('copy', function(event){
+    alert('Anda telah menyalin teks');    
+  })
+
+  document.getElementById('inputPaste').addEventListener('paste', function(event){
+    alert(' Anda telah menempelkan teks');
+  })
+  
 });
 
